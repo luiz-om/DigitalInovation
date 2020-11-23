d53ef7f111597e923d9df59bc1aa698e2900235b
Luiz Fernando de Oliveira Martins <luizfernando_martins@hotmail.com>
2020-11-23 18:06:44 -0400

    Criação Projeto

---
+++ CursoMVCC/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVCC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+
+
+
+        public List<Produto> produtos { get; set; }
+    }
+}
---
+++ CursoMVCC/Models/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVCC.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Data Source=DESKTOP-SVU1JRO\SQLLUIZ;Database=CursoMVCC;Integrated Security=True");
+        }
+    }
+}
